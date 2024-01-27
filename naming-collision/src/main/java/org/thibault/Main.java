ac9d91c06a32d98dc499e0d58479e3f9c3e71126
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-27 18:55:14 +0100

    App functional. To be tested.

--- naming-collision/src/main/java/org/thibault/Main.java
+++ naming-collision/src/main/java/org/thibault/Main.java
@@ -6,17 +6,36 @@ import org.thibault.command.Hello;
 import org.thibault.command.Time;
 import org.thibault.config.AppConfig;
 
+import java.util.Scanner;
+
 public class Main {
   
   public static void main(String[] args) {
     
+    Scanner scan = new Scanner (System.in);
     ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     
     Hello hello = context.getBean(Hello.class);
     Time time = context.getBean(Time.class);
     
-    hello.execute();
-    time.execute();
-    
+    while (true){
+      System.out.println("Please enter one of the commands. hello or time.");
+      System.out.println("Press enter (no input) to quit.");
+      System.out.print("> ");
+      String input = scan.nextLine().toLowerCase().trim();
+      
+      switch (input){
+        case "hello":
+          hello.execute();
+          break;
+        case "time":
+          time.execute();
+          break;
+        case "":
+          return;
+        default:
+          System.out.println(input + " is not a valid command. Please try again");
+      }
+    }
   }
 }
